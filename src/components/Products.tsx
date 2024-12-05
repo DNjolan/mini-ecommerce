b2347b350f0d8b664df8ea1361f085d5b41e40a2
DNjolan <dnjolan546@gmail.com>
2024-12-05 18:50:27 -0300

    ADD mostrar una lista de productos desde un JSON y ADD Estilo y responsive

---
+++ src/components/Products.tsx
@@ -0,0 +1,32 @@
+import '../styles/Products.css'
+type TypePatherProducts = {
+    products: Array<TypeProducts>
+}
+
+type TypeProducts = {
+    id: number,
+    title: string,
+    price: number,
+    images: string
+}
+
+export const Products: React.FC<TypePatherProducts> = ({ products }) => {
+    return(
+        <main className="containerMain">
+            <ul>
+                {products.map((product) => {
+                    return(
+                        <li key={product.id}>
+                            <h3>{product.title}</h3>
+                            <img
+                                src={product.images}
+                                alt={product.title}
+                            />
+                            <p>Precio: ${product.price}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </main>
+    )
+}
