9dfd650d172a7cf177a0175c6743137357a2888a
DNjolan <dnjolan546@gmail.com>
2024-12-06 16:24:26 -0300

    Add custome hook useFilter

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import { useState } from "react";
 import { Header } from "./components/Header";
 import { Products } from "./components/Products";
-// DB
-import { products } from './products/products.json'
+// CustomeHooks
+import useFilter from "./hooks/useFilter";
 
 export default function App() {
-  const [filter, setFilter] = useState({
-      price: 0,
-      category: 'All'
-  })
-
-  const handleChangePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFilter(prevState => ({
-          ...prevState,
-          price: Number(e.target.value)
-      }))
-      console.log(e.target.value)
-  }
-
-  const handleChangeCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
-      setFilter(prevState => ({
-        ...prevState,
-        category: e.target.value
-    }))
-      console.log(e.target.value)
-  }
-
-  const mappedFilterProducts = products.filter(product => {return (
-    product.price >= filter.price
-    && (product.category === filter.category || filter.category === 'All')
-  )})
-
+  const {filter, handleChangeCategory, handleChangePrice, mappedFilterProducts} = useFilter()
+  
   return(
     <>
     <Header filter={filter} handleChangePrice={handleChangePrice} handleChangeCategory={handleChangeCategory}/>
--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,26 +1,33 @@
 import { useState } from "react"
+// DB
 
+import { products } from '../products/products.json'
 export default function useFilter() {
     const [filter, setFilter] = useState({
         price: 0,
         category: 'All'
     })
-
+  
     const handleChangePrice = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFilter({
-            ...filter,
+        setFilter(prevState => ({
+            ...prevState,
             price: Number(e.target.value)
-        })
+        }))
         console.log(e.target.value)
     }
-
+  
     const handleChangeCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setFilter({
-            ...filter,
-            category: e.target.value
-        })
+        setFilter(prevState => ({
+          ...prevState,
+          category: e.target.value
+      }))
         console.log(e.target.value)
     }
 
-    return {filter, setFilter, handleChangePrice, handleChangeCategory}
+    const mappedFilterProducts = products.filter(product => {return (
+        product.price >= filter.price
+        && (product.category === filter.category || filter.category === 'All')
+    )})
+
+    return {filter, setFilter, handleChangePrice, handleChangeCategory, mappedFilterProducts}
 }
